876836609be43969784ae73e2d082190228cfdb6
VasylMolnar <108887823+VasylMolnar@users.noreply.github.com>
2023-05-16 10:16:06 +0300

    Update fetchCards.js

--- src/services/fetchCards.js
+++ src/services/fetchCards.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchCards = async (search, page) => {
   const url = 'https://pixabay.com/api/';
-  const key = '29730339-d122beaa10b573098fe13e924';
+  const key = 'add key';
   const filter = `?key=${key}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
   return await axios.get(`${url}${filter}`).then(response => response.data);
